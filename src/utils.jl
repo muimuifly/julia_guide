14539f70d7007cd0393c6dec6f5166a04cf5336a
muimuifly <mumufly2001@163.com>
2024-09-19 19:39:56 -0400

    Package function

---
+++ src/utils.jl
@@ -0,0 +1,25 @@
+using Test
+using Random
+function insertion_sort!(array::Array{Int, 1})
+    for i in 2:length(array)
+        key = array[i]
+        j = i - 1
+        
+        # Move elements of array[1..i-1], that are greater than key,
+        # to one position ahead of their current position
+        while j >= 1 && array[j] > key
+            array[j + 1] = array[j]
+            j -= 1
+        end
+        array[j + 1] = key
+    end
+end
+
+function check_sort(N::Int = 100)
+    array = collect(1:N)
+    shuffle!(array)
+    insertion_sort!(array)
+    return array == collect(1:N)
+end
+
+export insertion_sort!, check_sort
